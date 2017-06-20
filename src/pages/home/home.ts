474b342efc4aab3fe4f8898ef8b6777974d71e22
AzriZzz <lenovorosak@gmail.com>
2017-06-20 13:58:51 +0800

    give ionic change cards

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ItemDetailsPage } from '../item-details/item-details';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  items = ['Item1','Item2','Item3'];
   constructor(public navCtrl: NavController) {
 
   }
+  selectItem(item){
+    this.navCtrl.push(ItemDetailsPage, { item: item });
+}
 
 }
---
+++ src/pages/item-details/item-details.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import { NavParams } from 'ionic-angular';
+
+@Component({
+    templateUrl: 'item-details.html'
+})
+export class ItemDetailsPage{
+    item;
+    constructor (navParams: NavParams){
+        this.item = navParams.get('item');
+
+    }
+}
